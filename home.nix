d789f2cd67546f40c5ced4f235286d7f1ad54233
xkraty <christian@campoli.me>
2025-12-14 00:12:28 +0000

    fix: move to config file

--- home.nix
+++ home.nix
@@ -28,6 +28,7 @@ in
     enable = true;
     enableSshSupport = true;
     defaultCacheTtl = 300;
+    pinentry.package = pkgs.pinentry-qt;
   };
 
   programs.git = {
@@ -68,83 +69,7 @@ in
   programs.waybar = {
     enable = true;
     
-    # Optional: Install extra icons for better visuals
-    package = pkgs.waybar.override {
-      enableModules = [ "custom" "hyprland" ];
-    };
-
-    # The actual JSON configuration goes here (modules, position, etc.)
-    settings = {
-      main = {
-        # General position/styling settings
-        position = "top";
-        height = 30;
-        layer = "top";
-        
-        # Modules to display on the left side
-        modules-left = [ "hyprland/workspaces" "hyprland/window" ];
-        
-        # Modules to display in the center
-        modules-center = [ "clock" ];
-        
-        # Modules to display on the right side
-        modules-right = [ "network" "pulseaudio" "backlight" "battery" "tray" ];
-        
-        # Module definitions
-        "hyprland/workspaces" = {
-          format = "{icon}";
-          format-icons = {
-            "1" = ""; # Terminal icon
-            "2" = ""; # Browser icon
-            "3" = ""; # Desktop icon
-            "urgent" = "";
-            "active" = "";
-            "default" = "";
-          };
-        };
-        
-        clock = {
-          format = " {:%H:%M:%S}";
-          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-        };
-
-        battery = {
-          states = {
-            good = 90;
-            warning = 30;
-            critical = 15;
-          };
-          format = "{icon} {capacity}%";
-          format-charging = " {capacity}%";
-          format-plugged = " {capacity}%";
-          format-alt = "{time} {icon}";
-          format-icons = [ "" "" "" "" "" ];
-        };
-
-        pulseaudio = {
-          format = "{icon} {volume}%";
-          format-muted = " {format_source}";
-          format-icons = {
-            headphone = "headphones";
-            handsfree = "phone";
-            default = [ "" "" "" ];
-          };
-        };
-
-        network = {
-          format-wifi = " {essid}";
-          format-ethernet = "link";
-          format-alt = "up {bandwidthUpBytes} down {bandwidthDownBytes}";
-        };
-        
-        # System Tray (for applications like Discord, Telegram, etc.)
-        tray = {
-          spacing = 10;
-        };
-        
-      };
-    };
-    
+    config = builtins.readFile ./dotfiles/waybar/config.json;
     # We won't add the CSS here, but you can later use:
     # style = builtins.readFile ./dotfiles/waybar/style.css;
   };
